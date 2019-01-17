ac73f45ca2d2f02603765215e9bd4d65dd3a0c8e
yami-beta <yami-beta@users.noreply.github.com>
2019-01-17 16:35:55 +0900

    Set user to firestore after login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,18 +1,26 @@
 import React, { useContext, useCallback } from "react";
+import firebase from "firebase/app";
+import "firebase/firestore";
 import { AppContext } from "../App";
 import { Login, LoginResult } from "../components/Login";
 
 const LoginPage = () => {
-  const { firebaseAuth, history } = useContext(AppContext);
+  const { firebaseAuth, history, firestore } = useContext(AppContext);
 
-  const handleLogin = useCallback((loginResult: LoginResult) => {
+  const handleLogin = useCallback(async (loginResult: LoginResult) => {
     if (loginResult.error) {
       // eslint-disable-next-line no-console
       console.log(loginResult.error);
       return;
     }
 
-    history.push("/");
+    const done = await setUserToFirestore(firestore, loginResult.user);
+    if (done) {
+      history.push("/");
+    } else {
+      // eslint-disable-next-line no-console
+      console.log("Failed to set user to Firestore");
+    }
   }, []);
 
   return (
@@ -21,6 +29,30 @@ const LoginPage = () => {
       <Login firebaseAuth={firebaseAuth} handleLogin={handleLogin} />
     </div>
   );
+};
+
+const setUserToFirestore = async (
+  firestore: firebase.firestore.Firestore,
+  user: firebase.User
+): Promise<boolean> => {
+  const userDoc = firestore.collection("users").doc(user.uid);
+  const userDocSnapshot = await userDoc.get();
+  let updateFields: any = {
+    email: user.email,
+    lastLoginTimestamp: firebase.firestore.FieldValue.serverTimestamp()
+  };
+  if (!userDocSnapshot.exists) {
+    updateFields.role = "operator";
+  }
+
+  return await userDoc
+    .set(updateFields, { merge: true })
+    .then(() => {
+      return true;
+    })
+    .catch(() => {
+      return false;
+    });
 };
 
 export { LoginPage };
