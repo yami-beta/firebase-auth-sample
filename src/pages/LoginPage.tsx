d8c01dc027b9ee35f8e05bf3e779e525eeac026b
yami-beta <yami-beta@users.noreply.github.com>
2019-01-17 14:30:09 +0900

    Refactor login handling

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,14 +1,24 @@
-import React, { useContext } from "react";
+import React, { useContext, useCallback } from "react";
 import { AppContext } from "../App";
-import { Login } from "../components/Login";
+import { Login, LoginResult } from "../components/Login";
 
 const LoginPage = () => {
   const { firebaseAuth, history } = useContext(AppContext);
 
+  const handleLogin = useCallback((loginResult: LoginResult) => {
+    if (loginResult.error) {
+      // eslint-disable-next-line no-console
+      console.log(loginResult.error);
+      return;
+    }
+
+    history.push("/");
+  }, []);
+
   return (
     <div>
       <h1>Login</h1>
-      <Login firebaseAuth={firebaseAuth} history={history} />
+      <Login firebaseAuth={firebaseAuth} handleLogin={handleLogin} />
     </div>
   );
 };
